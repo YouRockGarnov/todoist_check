4c1d58b9870a59c9e7701da986f8e61b24a19ef5
Yury Garnov <hamta@yandex.ru>
2018-08-25 11:53:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,16 @@ app = Flask(__name__)
 def receive():
     print(request.args)
 
+    import json
+
+    r = (requests.post('https://todoist.com/oauth/access_token', {"client_id": "fb26051eb06649bb968791f3d7c2f185",
+                                                             "client_secret": "f912232a8fe14128a74bb36577aa6a6f",
+                                                             "code": request.args["code"]}))
+
+    obj = json.loads(r.content)
+    # print(requests.get("https://beta.todoist.com/API/v8/projects",
+    #                    headers={"Authorization": "Bearer %s" % obj['access_token']}).json())
+
+    print(todoist.login_with_api_token(obj['access_token']))
+    
     return 200
